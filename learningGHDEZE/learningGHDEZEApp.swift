628dccab1632d5c1c632fa1b6e974ca063d974e3
Koen Sas <koen@ivision.be>
2022-10-09 22:01:37 +0200

    Initial Commit

---
+++ learningGHDEZE/learningGHDEZEApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningGHDEZEApp.swift
+//  learningGHDEZE
+//
+//  Created by Koen Sas on 09/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct learningGHDEZEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
